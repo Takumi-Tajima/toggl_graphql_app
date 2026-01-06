17989169d41d5fdd886337472bfade0df9ec933c
taji <taji@sonicgarden.jp>
2026-01-06 22:56:20 +0900

    queryでデータ取得

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,16 +2,16 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
-      argument :id, ID, required: true, description: "ID of the object."
+    field :node, Types::NodeType, null: true, description: 'Fetches an object given its ID.' do
+      argument :id, ID, required: true, description: 'ID of the object.'
     end
 
     def node(id:)
       context.schema.object_from_id(id, context)
     end
 
-    field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
-      argument :ids, [ID], required: true, description: "IDs of the objects."
+    field :nodes, [Types::NodeType, { null: true }], null: true, description: 'Fetches a list of objects given a list of IDs.' do
+      argument :ids, [ID], required: true, description: 'IDs of the objects.'
     end
 
     def nodes(ids:)
@@ -23,9 +23,18 @@ module Types
 
     # TODO: remove me
     field :test_field, String, null: false,
-      description: "An example field added by the generator"
+                               description: 'An example field added by the generator'
     def test_field
-      "Hello World!"
+      'Hello World!'
+    end
+
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+    def user(id:)
+      User.find(id)
     end
   end
 end
+
+# https://zenn.dev/igaiga/books/rails-practice-note/viewer/rails_graphql_workshop#1%E3%81%A4%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B-field-%3Auser
